1b12d6fc7d6d5cf4d230cf7cd2e40a63d38bfe09
SecretiveCat <kristina2008.ua@gmail.com>
2026-06-28 19:52:29 +0300

    Complete task2, where I create Procedure: Create New Order

--- Assignment_03.sql
+++ Assignment_03.sql
@@ -24,6 +24,27 @@ select calculate_order_total(order_id) as Total
 from orders;
 
 
+---Таск 2, створення процедури
+
+create or replace procedure create_order(p_customer_id int)
+language plpgsql
+as $$
+begin 
+  if not exists (select from customers where customer_id = p_customer_id) then
+    raise exception 'Sorry, we do not fount this customer';
+  end if;
+    
+    insert into orders (customer_id, order_date, total_amount)
+    values (p_customer_id, CURRENT_TIMESTAMP, 0);
+end;
+$$;
+
+
+call create_order(3);
+
+SELECT * FROM orders ORDER BY order_id DESC LIMIT 5;
+
+
 
 
 
