743d816a7709e910886bed102764d62cf12483a6
SecretiveCat <kristina2008.ua@gmail.com>
2026-06-28 20:42:47 +0300

    Complete task3, where I create Procedure: Add Product to Order

--- Assignment_03.sql
+++ Assignment_03.sql
@@ -42,7 +42,46 @@ $$;
 
 call create_order(3);
 
-SELECT * FROM orders ORDER BY order_id DESC LIMIT 5;
+select * from orders
+order by order_id desc
+limit 5;
+
+
+create or replace procedure add_product_to_order(
+    p_order_id int,
+    p_product_id int,
+    p_quantity int
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	if p_quantity <= 0 then
+		raise exception 'Sorry, but quantity must be more then 0';
+	end if;
+
+	if (select stock_quantity from products where product_id = p_product_id) < p_quantity then
+		raise exception 'Sorry, but products are not enough stock.';
+	end if;
+
+    INSERT INTO order_items (order_id, product_id, quantity, price)
+    VALUES (p_order_id, p_product_id, p_quantity, (select price from products where product_id = p_product_id));
+
+	update products
+	set stock_quantity = stock_quantity - p_quantity
+	where product_id = p_product_id;
+end;
+$$;
+
+
+call add_product_to_order(3, 3, 10);
+
+
+select * from order_items 
+where order_id = 3;
+
+select product_id, stock_quantity 
+from products 
+where product_id = 3;
 
 
 
